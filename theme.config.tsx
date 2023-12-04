98a34f774453701d1bcb288745a1c1e89e851d6e
Devin Elliot <devinjelliot@gmail.com>
2023-12-03 20:21:58 -0800

    [WIP] partial fix for head tag

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,9 +1,11 @@
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Backpack Network</span>,
   project: {
     link: 'https://github.com/anotheragency/docs-backpack-network',
+
   },
   chat: {
     link: 'https://discord.gg/gVVBVrG9mf',
@@ -12,6 +14,26 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Backpack Network © 2023',
   },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://localhost:3000' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    return (
+      <>
+        <title>{frontMatter.title || 'Backpack Network'}</title> {/* Add this line */}
+
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Backpack Network'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Self banking with fUX'}
+        />
+      </>
+    )
+  }
 }
 
 export default config
